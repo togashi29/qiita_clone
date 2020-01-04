f1feaeeacfb24d6ccc5a35c3051c905521d61376
togashi29 <saladwithsalad@yahoo.co.jp>
2020-01-04 15:18:06 +0900

    article modelのfactoryとtestを作成

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :article do
-    title { "MyString" }
-    body { "MyText" }
-    user { nil }
+    title { Faker::Lorem.characters(number: 10) }
+    body { Faker::Lorem.paragraph }
+    association :user
   end
 end
