a2c0233934ee1509097b23da2628439d3b57ebd9
togashi29 <saladwithsalad@yahoo.co.jp>
2020-01-05 20:14:21 +0900

    spec/requests/articles_spec.rbを作成

---
+++ spec/requests/articles_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe "Api::V1::Articles", type: :request do
+  describe "GET /api/v1/articles" do
+    subject { get(api_v1_articles_path) }
+
+    before do
+      create_list(:article, 3)
+    end
+
+    it "記事の一覧が取得できる" do
+      subject
+      res = JSON.parse(response.body)
+
+      expect(res.length).to eq 3
+      expect(res[0].keys).to eq ["id", "title", "body", "user"]
+      expect(res[0]["user"].keys).to eq ["id", "name"]
+      expect(response).to have_http_status(200)
+    end
+  end
+end
