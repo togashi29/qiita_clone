ccceccae766cab9e42fe0148a3d38195af5476e6
togashi29 <saladwithsalad@yahoo.co.jp>
2020-01-05 22:32:43 +0900

    記事詳細に関するテストをspec/requests/articles_spec.rbに作成

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -18,4 +18,38 @@ RSpec.describe "Api::V1::Articles", type: :request do
       expect(response).to have_http_status(:ok)
     end
   end
+
+  describe "GET /api/v1/articles/:id" do
+    subject { get(api_v1_article_path(article_id)) }
+
+    describe "正常系のテスト" do
+      context "指定したidの記事が存在する場合" do
+        let(:article) { create(:article) }
+        let(:article_id) { article.id }
+
+        it "指定したidの記事が取得できる" do
+          subject
+          res = JSON.parse(response.body)
+
+          expect(res["id"]).to eq article.id
+          expect(res["title"]).to eq article.title
+          expect(res["body"]).to eq article.body
+          expect(res["user"]["id"]).to eq article.user.id
+          expect(res["user"]["name"]).to eq article.user.name
+          expect(response).to have_http_status(200)
+        end
+      end
+    end
+
+    describe "異常系のテスト" do
+      context "指定したidの記事が存在しない場合" do
+        let(:article_id) { 10000 }
+
+        it "記事が見つからない" do
+          expect { subject }.to raise_error ActiveRecord::RecordNotFound
+        end
+      end
+    end
+  end
+
 end
