649e2e40b965b7ddc4d4eabb76fae70b57d0192b
togashi29 <saladwithsalad@yahoo.co.jp>
2020-01-12 11:30:51 +0900

    記事作成APIに関するテストを実装

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -51,4 +51,23 @@ RSpec.describe "Api::V1::Articles", type: :request do
       end
     end
   end
+
+
+  describe "POST /api/v1/articles" do
+    subject { post(api_v1_articles_path, params: params) }
+    let(:params) {{ article: attributes_for(:article) }}
+    let(:current_user) { create(:user) }
+
+    before do
+      allow_any_instance_of(Api::V1::ApiController).to receive(:current_user).and_return(current_user)
+    end
+
+    it "記事が作成できる" do
+      expect { subject }.to change { current_user.articles.count }.by(1)
+      res = JSON.parse(response.body)
+      expect(res["title"]).to eq params[:article][:title]
+      expect(res["body"]).to eq params[:article][:body]
+      expect(response).to have_http_status(200)
+    end
+  end
 end
