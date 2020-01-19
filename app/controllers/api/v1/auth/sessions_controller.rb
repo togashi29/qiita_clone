20fc0ee49fd1b9800ef5bd2f8c0c5493c9eb5172
togashi29 <saladwithsalad@yahoo.co.jp>
2020-01-19 17:15:10 +0900

    binding.pryの記述を削除

--- app/controllers/api/v1/auth/sessions_controller.rb
+++ app/controllers/api/v1/auth/sessions_controller.rb
@@ -1,11 +1,2 @@
 class Api::V1::Auth::SessionsController < DeviseTokenAuth::SessionsController
-  # def params_for_resource(resource)
-  #   binding.pry
-  # end
-
-
-
-  def create
-    binding.pry
-  end
 end
