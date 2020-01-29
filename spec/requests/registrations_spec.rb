e8595c3e7e8ccc99928f4afda9849f2314a43d40
togashi29 <saladwithsalad@yahoo.co.jp>
2020-01-29 22:49:15 +0900

    registrationsのテストを実装

--- spec/requests/registrations_spec.rb
+++ spec/requests/registrations_spec.rb
@@ -1,9 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe "Registrations", type: :request do
-  describe "GET /registrations" do
-    it "works! (now write some real specs)" do
-      get registrations_path
+RSpec.describe "Api::V1::Auth::Registrations", type: :request do
+  describe "POST /api/v1/auth" do
+    subject { post(api_v1_user_registration_path, params: params) }
+    let(:params) { attributes_for(:user) }
+
+    it "ユーザーが作成されトークン情報が取得できる" do
+      subject
+      header = response.header
+      expect(header["access-token"]).to be_present
+      expect(header["client"]).to be_present
+      expect(header["expiry"]).to be_present
+      expect(header["uid"]).to be_present
       expect(response).to have_http_status(200)
     end
   end
