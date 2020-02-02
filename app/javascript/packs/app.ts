e39329dc02c87e8c75367eff9f13862d9eb7d154
togashi29 <saladwithsalad@yahoo.co.jp>
2020-02-03 08:52:27 +0900

    Add Header

--- app/javascript/packs/app.ts
+++ app/javascript/packs/app.ts
@@ -1,9 +1,13 @@
-import Vue from 'vue/dist/vue.esm'
-import Router from './router/router'
+import Vue from 'vue/dist/vue.esm';
+import Router from './router/router';
+import Header from "./container/Header.vue";
 
 document.addEventListener('turbolinks:load', () => {
   new Vue({
     el: '#app',
     router: Router,
-  })
-})
+    components: {
+      navbar: Header
+    }
+  });
+});
