221296f72f64a2738de085f890cddb021159184a
togashi29 <saladwithsalad@yahoo.co.jp>
2020-02-05 17:21:46 +0900

    draftのルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,10 @@ Rails.application.routes.draw do
         sessions: "api/v1/auth/sessions",
       }
       resources :articles
+
+      namespace :articles do
+        resources :draft
+      end
     end
   end
 end
