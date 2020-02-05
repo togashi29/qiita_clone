b9bc9b590db12eb3aeb38fcac6f831db3f76a4b5
togashi29 <saladwithsalad@yahoo.co.jp>
2020-02-05 09:33:00 +0900

    マイグレーションを実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_19_021717) do
+ActiveRecord::Schema.define(version: 2020_02_05_002917) do
 
   create_table "article_comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "body"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2020_01_19_021717) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status", default: "draft"
     t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
