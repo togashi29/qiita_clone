aa6ee3cd4b5723e596b86a2bbab81f1172080077
togashi29 <saladwithsalad@yahoo.co.jp>
2020-02-05 22:18:07 +0900

    テストを追加

--- spec/requests/api/v1/articles/drafts_spec.rb
+++ spec/requests/api/v1/articles/drafts_spec.rb
@@ -21,4 +21,29 @@ RSpec.describe "Api::V1::Articles::Drafts", type: :request do
       end
     end
   end
+
+  describe "GET /api/v1/articles/drafts/:id" do
+    subject { get(api_v1_articles_draft_path(article.id), headers: headers) }
+
+    let(:current_user) { create(:user) }
+    let(:headers) { current_user.create_new_auth_token }
+
+    context "ログインユーザーが作成した下書き記事があるとき" do
+      let(:article) { create(:article, user: current_user) }
+
+      it "指定したidの下書き記事が取得できる" do
+        subject
+        res = JSON.parse(response.body)
+
+        expect(res["id"]).to eq article.id
+        expect(res["title"]).to eq article.title
+        expect(res["body"]).to eq article.body
+        expect(res["updated_at"]).to be_present
+        expect(res["status"]).to eq article.status
+        expect(res["user"]["id"]).to eq article.user.id
+        expect(res["user"]["name"]).to eq article.user.name
+        expect(response).to have_http_status(:ok)
+      end
+    end
+  end
 end
