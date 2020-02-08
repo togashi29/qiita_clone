000730e23466e536aac7210f5e223004a00b8577
togashi29 <saladwithsalad@yahoo.co.jp>
2020-02-08 21:50:16 +0900

    traitを使ってFactoryの場合分け

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -4,5 +4,13 @@ FactoryBot.define do
     body { Faker::Quote.matz }
     status { :draft }
     user
+
+    trait :draft do
+      status { :draft }
+    end
+
+    trait :published do
+      status { :published }
+    end
   end
 end
