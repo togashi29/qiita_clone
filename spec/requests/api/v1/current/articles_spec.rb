9caa05f0b141e7c48de02c9969ba5440661da4bc
togashi29 <saladwithsalad@yahoo.co.jp>
2020-02-08 21:51:50 +0900

    テストレコード数及びパターンを修正

--- spec/requests/api/v1/current/articles_spec.rb
+++ spec/requests/api/v1/current/articles_spec.rb
@@ -9,15 +9,16 @@ RSpec.describe "Api::V1::Current::Articles", type: :request do
 
     context "自分が書いた公開記事があるとき" do
       before do
-        create_list(:article, 5, status: "published", user: current_user)
-        create_list(:article, 10, user: current_user)
+        create_list(:article, 1, :draft, user: current_user)
+        create_list(:article, 2, :published)
+        create_list(:article, 4, :published, user: current_user)
       end
 
       it "自分が書いた公開記事の一覧を取得できる" do
         subject
         res = JSON.parse(response.body)
 
-        expect(res.length).to eq 5
+        expect(res.length).to eq 4
         expect(res[0].keys).to eq ["id", "title", "body", "updated_at", "status", "user"]
         expect(res[0]["user"].keys).to eq ["id", "name"]
         expect(response).to have_http_status(:ok)
