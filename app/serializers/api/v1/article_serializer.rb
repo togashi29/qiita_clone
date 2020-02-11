2bb82dc050008e1516b20be22184bf0a2b6619cb
togashi29 <saladwithsalad@yahoo.co.jp>
2020-02-11 22:41:32 +0900

    serializerを修正

---
+++ app/serializers/api/v1/article_serializer.rb
@@ -0,0 +1,5 @@
+class Api::V1::ArticleSerializer < ActiveModel::Serializer
+  attributes :id, :title, :body, :updated_at, :status
+  belongs_to :user, serializer: Api::V1::UserSerializer
+  end
+end
--- app/serializers/article_serializer.rb
+++
@@ -1,8 +0,0 @@
-class ArticleSerializer < ActiveModel::Serializer
-  attributes :id, :title, :body, :updated_at, :status
-  belongs_to :user
-  # user model の一部の属性のみ返したいため
-  class UserSerializer < ActiveModel::Serializer
-    attributes :id, :name
-  end
-end
