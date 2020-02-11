b8f9214ae96c1433e0f7c14b7fdde18b1e718cb0
togashi29 <saladwithsalad@yahoo.co.jp>
2020-02-11 22:24:03 +0900

    マイページを追加（ただし自分で書いた公開記事を確認できるのみ）

--- app/javascript/packs/container/Header.vue
+++ app/javascript/packs/container/Header.vue
@@ -4,17 +4,25 @@
       <v-toolbar-title class="white--text font-weight-bold">Qiita</v-toolbar-title>
     </router-link>
 
-    <!-- <v-btn icon>
-      <v-icon>search</v-icon>
-    </v-btn>-->
-
     <v-spacer></v-spacer>
 
     <div v-if="isLoggedIn">
       <router-link to="/articles/new" class="header-link">
         <v-btn flat class="post font-weight-bold">投稿する</v-btn>
       </router-link>
-      <v-btn flat @click="logout" class="white--text font-weight-bold">ログアウト</v-btn>
+      <v-menu bottom left min-width="120">
+        <template v-slot:activator="{ on }">
+          <v-btn dark icon v-on="on">
+            <v-icon>more_vert</v-icon>
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-tile v-for="(menu, i) in menus" :key="i" @click="undefined">
+            <v-list-tile-title @click="menu.click">{{ menu.title }}</v-list-tile-title>
+          </v-list-tile>
+        </v-list>
+      </v-menu>
     </div>
     <div v-else>
       <router-link to="/sign_up" class="header-link">
@@ -31,6 +39,7 @@
 import axios from "axios";
 import { Vue, Component } from "vue-property-decorator";
 import Router from "../router/router";
+
 const headers = {
   headers: {
     Authorization: "Bearer",
@@ -40,9 +49,37 @@ const headers = {
     uid: localStorage.getItem("uid")
   }
 };
+
 @Component
 export default class Header extends Vue {
   isLoggedIn: boolean = !!localStorage.getItem("access-token");
+  items: any = [
+    { title: "Menu" },
+    { title: "Menu" },
+    { title: "Menu" },
+    { title: "Menu" }
+  ];
+  menus: any = [
+    {
+      title: "マイページ",
+      click: () => {
+        this.moveToMyPage();
+      }
+    },
+    {
+      title: "下書き一覧",
+      click: () => {
+        this.moveToDrafts();
+      }
+    },
+    {
+      title: "ログアウト",
+      click: () => {
+        this.logout();
+      }
+    }
+  ];
+
   async logout(): Promise<void> {
     await axios
       .delete("/api/v1/auth/sign_out", headers)
@@ -52,11 +89,21 @@ export default class Header extends Vue {
       .catch(e => {
         // TODO: 適切な Error 表示
         alert(e.response.data.errors);
+
         // localStorage は残っているが、
         // ログアウトはしてしまっている状態なのですべてリセットする
         this.refresh();
       });
   }
+
+  moveToMyPage(): void {
+    Router.push("/mypage");
+  }
+
+  moveToDrafts(): void {
+    Router.push("/articles/drafts");
+  }
+
   private refresh(): void {
     localStorage.clear();
     Router.push("/");
@@ -70,11 +117,13 @@ export default class Header extends Vue {
 .header-link {
   text-decoration: none;
 }
+
 .register,
 .post {
   border: 2px solid #fff;
   border-radius: 5px;
 }
+
 .login {
   font-weight: bold;
 }
--- app/javascript/packs/router/router.ts
+++ app/javascript/packs/router/router.ts
@@ -1,21 +1,27 @@
-import Vue from 'vue/dist/vue.esm.js'
-import VueRouter from 'vue-router'
-import ArticlesContainer from '../components/ArticleList.vue'
-import ArticleContainer from "../container/ArticleContainer.vue"
-import RegisterContainer from "../container/RegisterContainer.vue"
-import LoginContainer from "../container/LoginContainer.vue"
-import EditArticleContainer from "../container/EditArticleContainer.vue"
+import Vue from "vue/dist/vue.esm.js";
+import VueRouter from "vue-router";
+import ArticlesContainer from "../container/ArticlesContainer.vue";
+import ArticleContainer from "../container/ArticleContainer.vue";
+import RegisterContainer from "../container/RegisterContainer.vue";
+import LoginContainer from "../container/LoginContainer.vue";
+import EditArticleContainer from "../container/EditArticleContainer.vue";
+import DraftArticlesContainer from "../container/DraftArticlesContainer.vue";
+import EditDraftArticleContainer from "../container/EditDraftArticleContainer.vue";
+import MyPageContainer from "../container/MyPageContainer.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 export default new VueRouter({
-  mode: 'history',
+  mode: "history",
   routes: [
     { path: "/", component: ArticlesContainer },
     { path: "/sign_up", component: RegisterContainer },
     { path: "/sign_in", component: LoginContainer },
     { path: "/articles/new", component: EditArticleContainer },
+    { path: "/articles/drafts", component: DraftArticlesContainer },
     { path: "/articles/:id/edit", component: EditArticleContainer },
-    { path: "/articles/:id", component: ArticleContainer, name: "article" }
+    { path: "/articles/drafts/:id/edit", component: EditDraftArticleContainer },
+    { path: "/articles/:id", component: ArticleContainer, name: "article" },
+    { path: "/mypage", component: MyPageContainer }
   ]
-})
+});
