f5d53a272df5353936d8fef9d7c985d9fad3cfa5
togashi29 <saladwithsalad@yahoo.co.jp>
2020-02-11 22:25:19 +0900

    MyPage のルーティングの修正

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   get "articles/drafts/:id/edit", to: "homes#index"
   get "articles/:id/edit", to: "homes#index"
   get "articles/:id", to: "homes#index"
-  get "articles/mypage", to: "homes#index"
+  get "mypage", to: "homes#index"
 
   namespace :api, format: :json do
     namespace :v1 do
